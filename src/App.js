df5c0fd725ecf4fcb55ef4140426bb938cccb7f7
WGNR8 <github@wgnr8.com.br>
2020-01-04 13:24:38 -0300

    feat: change main page title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Netilify hosted ReactJS application.
         </p>
         <a
           className="App-link"
